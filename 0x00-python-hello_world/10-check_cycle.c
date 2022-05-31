02a86ec4d6098fcc5408171fcdc8756fe7e9cc5a
Abzed <ibraabzed@gmail.com>
2022-05-31 10:13:06 +0300

    Fix with wsl

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,25 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list: singly list to check
- * Return: 0 if there is no cycle, 1 if there is a cycle
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-int check_cycle(listint_t *list)
+int main(void)
 {
+	listint_t *head;
 	listint_t *current;
+	listint_t *temp;
+	int i;
 
-	if (list == NULL)
-		return (0);
-	
-	current = list->next;
+	head = NULL;
+	add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	add_nodeint(&head, 98);
+	add_nodeint(&head, 402);
+	add_nodeint(&head, 1024);
+	print_listint(head);
 
-	while (current != NULL)
-	{
-		if (current == list)
-			return (1);
+	if (check_cycle(head) == 0)
+		printf("Linked list has no cycle\n");
+	else if (check_cycle(head) == 1)
+		printf("Linked list has a cycle\n");
+
+	current = head;
+	for (i = 0; i < 4; i++)
 		current = current->next;
-	}
+	temp = current->next;
+	current->next = head;
+
+	if (check_cycle(head) == 0)
+		printf("Linked list has no cycle\n");
+	else if (check_cycle(head) == 1)
+		printf("Linked list has a cycle\n");
+
+	current = head;
+	for (i = 0; i < 4; i++)
+		current = current->next;
+	current->next = temp;
+
+	free_listint(head);
 
 	return (0);
 }
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-number = 3.14159
+
+number = 3.14366655
 print("Float: {:.2f}".format(number))
